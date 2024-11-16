a29489d0154b8ac91e78f2f873b709661e85d959
toony9999 <160816960+toony9999@users.noreply.github.com>
2024-11-16 11:01:35 +0800

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -2,7 +2,7 @@ import socket
 
 def start_udp_client(host='127.0.0.1', port=8888):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.sendto("Hello from the client".encode(), (host, port))
+    client_socket.sendto("Hello from the client\n".encode(), (host, port))
     
     # Optionally, receive a response
     data, addr = client_socket.recvfrom(1024)
